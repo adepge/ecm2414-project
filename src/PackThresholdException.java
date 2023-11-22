2ff1e2c8b2da09e1864b3c54bed6dbdaa40829cd
Astr0guy <benellison2003@gmail.com>
2023-11-22 15:21:57 +0000

    21/11/23
    Time: 15:30-18:30
    Driver: Ben
    Observer: Adam
    
    Tasks completed:
    Fixed output
    Introduced rollback feature

---
+++ src/PackThresholdException.java
@@ -0,0 +1,8 @@
+public class PackThresholdException extends Exception {
+
+    public PackThresholdException() {
+    }
+	public PackThresholdException(String message) {
+            super(message);
+    }
+}
